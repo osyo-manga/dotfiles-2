44155c423696da6af8a3b2d287084b6c780aaab4
Giacomo Comitti <giacomit@gmail.com>
2013-12-28 17:22:02 +0100

    Update zsh stuff

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,7 +1,7 @@
 
 # path
 
-export PATH=$HOME/bin:$PATH
+export PATH=$HOME/opt/bin:$PATH
 export PATH=/bin:$PATH
 export PATH=/usr/bin:$PATH
 export PATH=/usr/local/bin:$PATH
@@ -10,7 +10,7 @@ export PATH="/Applications/Android Studio.app/sdk/tools:$PATH"
 
 export GOROOT=$(brew --prefix go)/libexec
 export GOPATH=$HOME/dropbox/dev/go:$GOPATH
-export GOPATH=$HOME/bin/go:$GOPATH
-export PATH=$HOME/bin/go/bin:$PATH
+export GOPATH=$HOME/opt/go:$GOPATH
+export PATH=$HOME/opt/go/bin:$PATH
 
 export MANPATH=$(brew --prefix coreutils)/libexec/gnuman:$MANPATH
